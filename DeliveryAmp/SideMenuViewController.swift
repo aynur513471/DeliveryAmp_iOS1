577fce1f8d5dd9129a8ab9dc4d42701f5c467211
AndreiBaches <andrei.baches@teamextension.ro>
2017-08-04 10:04:32 +0300

    changed file names

--- DeliveryAmp/SideMenuViewController.swift
+++ DeliveryAmp/SideMenuViewController.swift
@@ -14,6 +14,7 @@ class SideMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Do any additional setup after loading the view.
     }
 
